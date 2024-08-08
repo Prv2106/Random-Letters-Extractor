9577adea58fc8f63dcee6c63f5b51e27c3d62419
Alberto Provenzano <101663958+Prv2106@users.noreply.github.com>
2024-08-08 09:30:20 +0000

    Modifiche estetiche, aggiunte animazioni

--- RLE.js
+++ RLE.js
@@ -1,63 +1,67 @@
-
-
-
-
-
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let extracted = [];
 
-function extractor(){
+function extractor() {
     let i = getRandomIntInRange(alphabet.length);
-    console.log('numero estratto',i);
-    console.log('Lunghezza array pirma estrazione',alphabet.length);
-    console.log('Lettera estratta',alphabet[i]);
+    console.log('numero estratto', i);
+    console.log('Lunghezza array prima estrazione', alphabet.length);
+    console.log('Lettera estratta', alphabet[i]);
 
     extracted.push(alphabet[i]);
     extracted_letter.textContent = alphabet[i];
-    alphabet.splice(i,1);
-    console.log('Lunghezza array dopo estrazione',alphabet.length);
-
-    show_extracted.textContent = extracted;
-    show_remaining.textContent = alphabet;
-    e_label.classList.remove('hidden');
-    remaining.classList.remove('hidden');    
-
-    if(alphabet.length === 0){
-        b_label.classList.add('hidden');
-        button.classList.add('hidden');
-    }
+    letterBox.classList.add('show');
     
+    alphabet.splice(i, 1);
+    console.log('Lunghezza array dopo estrazione', alphabet.length);
+
+    document.querySelector('#remainingLetters span').textContent = alphabet.join(', ');
+    document.querySelector('#extractedLetters span').textContent = extracted.join(', ');
+    
+    e_label.classList.remove('hidden');
+    show_extracted.classList.remove('hidden');
+
+    if (alphabet.length === 0) {
+        // b_label.classList.add('hidden');
+        b_label.textContent = "Premi Restart per ricominciare";
+        button.classList.add('hidden');
+        show_remaining.classList.add('hidden');
+    }
 }
 
 function getRandomIntInRange(max) {
     return Math.floor(Math.random() * max);
 }
-  
 
-
-function Restart(){
-    if(alphabet.length === 0){
-        b_label.classList.remove('hidden');
+function Restart() {
+    if (alphabet.length === 0) {
+        // b_label.classList.remove('hidden');
+        b_label.textContent = "Estrai una lettera";
         button.classList.remove('hidden');
+        show_remaining.classList.remove('hidden');
+
     }
     extracted_letter.textContent = '';
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    show_remaining.textContent = alphabet;
+
+    document.querySelector('#remainingLetters span').textContent = alphabet.join(', ');
     e_label.classList.add('hidden');
-    remaining.classList.add('hidden');
+    show_extracted.classList.add('hidden');
+    extracted = [];
+    letterBox.classList.remove('hidden');
+    letterBox.classList.remove('show');
 }
 
-
-
 let button = document.querySelector('#Extractor');
-button.addEventListener('click',extractor);
+button.addEventListener('click', extractor);
 let e_label = document.querySelector("#Extracted_label");
 let b_label = document.querySelector("#button_label");
 let remaining = document.querySelector("#remaining");
+
 let extracted_letter = document.querySelector('#extractedLetter');
 let show_extracted = document.querySelector('#extractedLetters');
 let show_remaining = document.querySelector('#remainingLetters');
-show_remaining.textContent = alphabet;
+document.querySelector('#remainingLetters span').textContent = alphabet.join(', ');
 
+let letterBox = document.querySelector('#letterBox');
 let restart = document.querySelector('#Restart');
-restart.addEventListener('click',Restart);
+restart.addEventListener('click', Restart);
